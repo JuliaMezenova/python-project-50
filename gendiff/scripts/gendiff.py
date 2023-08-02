04dcca3ce42faffb325e6ac8fcd1114076e68863
Julia Mezenova <julia21moon86@mail.ru>
2023-08-02 12:37:41 +0500

    Add first and second steps of project

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+
+import argparse
+parser = argparse.ArgumentParser(description="Compares two configuration files and shows a difference.")
+parser.add_argument("first_file")
+parser.add_argument("second_file")
+parser.add_argument("-f", "--format", help="set format of output")
+args = parser.parse_args()
+
+
+def main():
+    args = parse()
+    print(args.first_file, args.second_file)
+
+
+if __name__ == '__main__':
+    main()
+
