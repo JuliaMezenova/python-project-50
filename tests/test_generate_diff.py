d0607bc877ba8b93774033babfe5c1820192b696
Julia Mezenova <julia21moon86@mail.ru>
2023-08-18 16:48:50 +0500

    add tests

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -15,16 +15,16 @@ def test_generate_diff():
     assert diff_yaml2 == open("tests/fixtures/correct_result_tree.txt").read()
     
     diff_json2_plain = generate_diff("tests/fixtures/file1_tree.json", "tests/fixtures/file2_tree.json", format_name='plain')
-    assert diff_json2_plain == open("tests/fixtures/correct_result_tree_plain.txt").read()
+    assert diff_json2_plain == open("tests/fixtures/correct_result_tree_plain.txt").read().strip()
     
-    diff_yaml2_plain == generate_diff("tests/fixtures/file1_tree.yaml", "tests/fixtures/file2_tree.yaml", format_name='plain')
-    assert diff_yaml2_plain == open("tests/fixtures/correct_result_tree_plain.txt").read()
+    diff_yaml2_plain = generate_diff("tests/fixtures/file1_tree.yaml", "tests/fixtures/file2_tree.yaml", format_name='plain')
+    assert diff_yaml2_plain == open("tests/fixtures/correct_result_tree_plain.txt").read().strip()
     
     diff_json3_json = generate_diff("tests/fixtures/file1_tree.json", "tests/fixtures/file2_tree.json", format_name='json')
-    assert diff_json3_plain == open("tests/fixtures/correct_result_tree_json.txt").read()
+    assert diff_json3_json == open("tests/fixtures/correct_result_tree_json.txt").read().strip()
     
-    diff_yaml3_plain == generate_diff("tests/fixtures/file1_tree.yaml", "tests/fixtures/file2_tree.yaml", format_name='json')
-    assert diff_yaml3_plain == open("tests/fixtures/correct_result_tree_json.txt").read()
+    diff_yaml3_json = generate_diff("tests/fixtures/file1_tree.yaml", "tests/fixtures/file2_tree.yaml", format_name='json')
+    assert diff_yaml3_json == open("tests/fixtures/correct_result_tree_json.txt").read().strip()
     
-    diff_json_to_json == generate_diff("tests/fixtures/file1.json", "tests/fixtures/file2.json")
-    assert diff_json_to_json == open("tests/fixtures/correct_result_json_format.txt").read()
+    diff_json_to_json = generate_diff("tests/fixtures/file1.json", "tests/fixtures/file2.json", format_name='json')
+    assert diff_json_to_json == open("tests/fixtures/correct_result_json_format.txt").read().strip()
